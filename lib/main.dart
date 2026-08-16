8fbf9b35469dc588062558aaec20345533ee1b74
polin <polinasemushevaa@gmail.com>
2026-08-16 12:58:20 +0500

    feat : integrate firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:kotik/app/app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kotik/core/app/app.dart';
+import 'package:kotik/firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(360, 844),
+      builder: (context, child) {
+        return MaterialApp(home: child);
+      },
+      child: const MyApp(),
+    ),
+  );
 }
